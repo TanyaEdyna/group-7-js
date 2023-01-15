8b6b5b4c3e4a0afbc49f6d3bd2f17ea1ab408e2c
Illia Datsenko <datsenko.i@gmail.com>
2023-01-15 11:15:15 +0200

    task2

--- js/index.js
+++ js/index.js
@@ -1,9 +1,19 @@
 // ЗАДАЧА 1
-// НАПИШІТЬ РЕЧЕННЯ ЗА ДОПОМОГОЮ ШАБЛОННОГО РЯДКА "ДОБРОГО ДНЯ,МЕНЕ ЗВАТИ ГАЛИНА, 
+// НАПИШІТЬ РЕЧЕННЯ ЗА ДОПОМОГОЮ ШАБЛОННОГО РЯДКА "ДОБРОГО ДНЯ,МЕНЕ ЗВАТИ ГАЛИНА,
 // МЕНІ 35 РОКІВ, Я ВИВЧАЮ JAVASCRIPT"
 
-const name = "Galyna";
-const age = 35;
-const hobby = "Java Script";
-const message = `ДОБРОГО ДНЯ,МЕНЕ ЗВАТИ ${name}, МЕНІ ${age} РОКІВ, Я ВИВЧАЮ ${hobby}`;
-console.log(message);
+// const name = "Galyna";
+// const age = 35;
+// const hobby = "Java Script";
+// const message = `ДОБРОГО ДНЯ,МЕНЕ ЗВАТИ ${name}, МЕНІ ${age} РОКІВ, Я ВИВЧАЮ ${hobby}`;
+// console.log(message);
+
+// ЗАДАЧА 2
+// ЗАПИТАТИ У КОРИСТУВАЧА ІМЯ, ПОЧТУ , ТЕЛЕФОН, І ПОКАЗАТИ У КОНСОЛІ "КОРИСТУВАЧ __ ВИКОРИСТОВУЄ  __ ПОЧТУ І __ ТЕЛЕФОН"
+
+// const name = prompt("Введіть своє ім'я");
+// const mail = prompt("Введіть свою електронну пошту");
+// const tel = prompt("Введіть свій телефон");
+
+// const message = `КОРИСТУВАЧ ${name} ВИКОРИСТОВУЄ  ${mail} ПОЧТУ І ${tel} ТЕЛЕФОН`;
+// console.log(message);
